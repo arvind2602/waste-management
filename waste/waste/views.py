298118416665d12aa019863579cc82ec838b452c
arvind2602 <guptaarvind2602@gmail.com>
2023-04-21 07:39:42 +0530

    Added views

--- waste/waste/views.py
+++ waste/waste/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from form import forms
 
 def index(request):
+    if request.method=="POST":
+        username=request.POST['name']
+        phone=request.POST['phone']
+        donation=request.POST['donation']
+        address=request.POST['address']
+        message=request.POST['text']
+        user=User.objects.create_user(username,phone,donation,address,message)
+        user.save()
+        return redirect(request,'/')
     return render(request,'index.html')
+def forms(request):
+    return render(request,)
